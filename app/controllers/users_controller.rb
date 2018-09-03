fb1c74942f7e563991481ae1a0241288cf785fef
thanhluanuit <thanhluan.uit@gmail.com>
2018-09-03 15:51:50 +0700

    Edit Profile Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    redirect_to edit_user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :name, :website, :bio, :email, :phone, :gender)
   end
 end
