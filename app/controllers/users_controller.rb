8cefc0f315c276d33daeb7efd3c023127890238b
thanhluanuit <thanhluan.uit@gmail.com>
2018-09-03 16:14:27 +0700

    User form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @user.update(user_params)
 
-    redirect_to edit_user_path(@user)
+    redirect_to @user
   end
 
   private
