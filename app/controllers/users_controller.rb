6adb8994ce9fcdf57da3541a46a72e42423f3454
thanhluanuit <thanhluan.uit@gmail.com>
2018-09-19 19:57:49 +0700

    Update small in css vs html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :name, :website, :bio, :email, :phone, :gender)
+    params.require(:user).permit(:username, :name, :website,
+                                 :bio, :email, :phone, :gender)
   end
 end
