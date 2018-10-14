3527e9823cebae52504796ad0b1533d4a2a9e0fb
thanhluanuit <thanhluan.uit@gmail.com>
2018-10-14 15:45:46 +0700

    Show posts in user profile page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    @posts = current_user.posts.order(created_at: :desc)
+    @posts = Post.all.order(created_at: :desc)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+    @posts = current_user.posts.order(created_at: :desc)
   end
 
   def edit
