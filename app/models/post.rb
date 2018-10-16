2bc03a462d84cc326cc44778655b57c0cb7da395
thanhluanuit <thanhluan.uit@gmail.com>
2018-10-16 20:35:32 +0700

    Add validation to Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
-
   has_one_attached :image
+
+  validate :image_presence
+
+  def image_presence
+    errors.add(:image, "can't be blank") unless image.attached?
+  end
 end
