e71e975061c24e25003a7be747aa417f6a1da5d0
thanhluanuit <thanhluan.uit@gmail.com>
2018-10-20 11:16:16 +0700

    Add a callback to create hashtags

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,8 +6,19 @@ class Post < ApplicationRecord
   has_many :hash_tags, through: :post_hash_tags
 
   validate :image_presence
+  after_commit :create_hash_tags, on: :create
 
   def image_presence
     errors.add(:image, "can't be blank") unless image.attached?
+  end
+
+  def create_hash_tags
+    extract_name_hash_tags.each do |name|
+      hash_tags.create(name: name)
+    end
+  end
+
+  def extract_name_hash_tags
+    description.to_s.scan(/#\w+/).map{|name| name.gsub("#", "")}
   end
 end
