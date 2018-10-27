1a5db066d87a9cd90e576f9e56999a5ef5d4f540
thanhluanuit <thanhluan.uit@gmail.com>
2018-10-27 15:26:55 +0700

    Add destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,13 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def destroy
+    @post = current_user.posts.find(params[:id])
+    @post.destroy
+
+    redirect_to user_path(current_user)
+  end
+
   private
 
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-  resources :posts, only: [:new, :create, :show]
+  resources :posts, only: [:new, :create, :show, :destroy]
   get 'search' => 'search#index'
 end
