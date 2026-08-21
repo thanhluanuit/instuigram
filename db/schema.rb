7d53322725ef1d988ea5f738ed5ac82c56f189b1
thanhluanuit <thanhluan.uit@gmail.com>
2026-08-21 08:58:34 +0700

    Add schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_08_18_135225) do
+ActiveRecord::Schema[8.0].define(version: 2026_08_18_144548) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -83,6 +83,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_08_18_135225) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.integer "reactions_count", default: 0, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "reactions", force: :cascade do |t|
