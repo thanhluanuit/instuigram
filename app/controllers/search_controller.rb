2ed65ad06d1ca24fcac1b1ef7590ca56c95f7943
thanhluanuit <thanhluan.uit@gmail.com>
2026-08-22 09:43:59 +0700

    Refactor code search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,10 +1,16 @@
 class SearchController < ApplicationController
   def index
-    response = Post.search(params[:query])
-    @posts = if response
-      response.page(params[:page]).per(5).records(includes: { image_attachment: :blob })
+    search_response = Post.search(search_params[:query])
+    @posts = if search_response
+      search_response.page(search_params[:page]).per(5).records(includes: { image_attachment: :blob })
     else
-      Kaminari.paginate_array([]).page(params[:page])
+      Kaminari.paginate_array([]).page(search_params[:page]).per(5)
     end
+  end
+
+  private
+
+  def search_params
+    params.permit(:query, :page)
   end
 end
