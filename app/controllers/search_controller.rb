b2429c6ca3dec7b5d8056396981c9a0067ce2ce2
thanhluanuit <thanhluan.uit@gmail.com>
2026-08-25 15:50:50 +0700

    Fix rubocop

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,9 +3,9 @@ class SearchController < ApplicationController
     search_response     = Post.search(search_params[:query])
     @posts              = if search_response
                             search_response.page(search_params[:page]).per(10).records(includes: { image_attachment: :blob })
-                          else
+    else
                             Kaminari.paginate_array([]).page(search_params[:page]).per(10)
-                          end
+    end
     @trending_hash_tags = HashTag.trending.to_a
     @suggested_users    = User.suggested_for(current_user).to_a
   end
