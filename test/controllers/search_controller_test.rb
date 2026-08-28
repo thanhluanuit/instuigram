fd1140ad33f8398b9c4fe6941e7a7d192ad427bd
thanhluanuit <thanhluan.uit@gmail.com>
2026-08-28 17:09:39 +0700

    Cover search result pagination, out-of-range pages, and multi-word queries

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -36,6 +36,32 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     assert_select "h1", "Oop! No matching posts ..."
   end
 
+  test "paginates matching posts 10 per page" do
+    index_matching_posts(11)
+
+    get search_path(query: "pagination marker")
+    assert_select ".user-images .wrapper", count: 10
+
+    get search_path(query: "pagination marker", page: 2)
+    assert_select ".user-images .wrapper", count: 1
+  end
+
+  test "with a page past the last one, shows no matching posts" do
+    index_matching_posts(11)
+
+    get search_path(query: "pagination marker", page: 99)
+
+    assert_response :success
+    assert_select "h1", "Oop! No matching posts ..."
+  end
+
+  test "with a multi-word query, finds the post whose description contains those words" do
+    get search_path(query: "walk beach")
+
+    assert_select "h1", "Top Posts"
+    assert_select "a[href=?]", post_path(posts(:two))
+  end
+
   test "avoids N+1 queries when rendering multiple matching posts" do
     perform_enqueued_jobs do
       create_post!(users(:one), description: "shared marker one")
@@ -47,5 +73,13 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
 
     assert_select "h1", "Top Posts"
     assert_select ".user-images .wrapper", count: 2
+  end
+
+  private
+
+  def index_matching_posts(count)
+    count.times { |n| create_post!(users(:one), description: "pagination marker #{n}") }
+    perform_enqueued_jobs
+    Post.__elasticsearch__.refresh_index!
   end
 end
