0f236b4fa9855e836fad9da1859dcd0a58ec59e8
thanhluanuit <thanhluan.uit@gmail.com>
2026-08-28 14:49:04 +0700

    Refresh comments, form and count over Turbo Streams

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,25 +2,36 @@ class CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    post = Post.find(params[:post_id])
-    comment = current_user.comments.create(comment_params.merge(post: post))
-    if comment.persisted?
-      log_event(event_type: :comment_created, subject: comment)
-      redirect_to post_path(post)
-    else
-      redirect_to post_path(post), alert: comment.errors.full_messages.to_sentence
+    @post    = Post.find(params[:post_id])
+    @comment = current_user.comments.create(comment_params.merge(post: @post))
+
+    unless @comment.persisted?
+      return redirect_to post_path(@post), alert: @comment.errors.full_messages.to_sentence
     end
+
+    log_event(event_type: :comment_created, subject: @comment)
+    render_post_comments
   end
 
   def destroy
     comment = current_user.comments.find(params[:id])
-    post_id = comment.post_id
+    @post   = comment.post
     comment.destroy
 
-    redirect_to post_path(post_id)
+    render_post_comments
   end
 
   private
+
+  def render_post_comments
+    respond_to do |format|
+      format.turbo_stream do
+        @post = Post.includes(comments: :user).find(@post.id)
+        render :create
+      end
+      format.html { redirect_to post_path(@post) }
+    end
+  end
 
   def comment_params
     params.require(:comment).permit(:body)
