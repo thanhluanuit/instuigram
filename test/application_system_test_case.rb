a4f4bae89a68e7b1c6d6a2604ef5e3f12dbf6a18
thanhluanuit <thanhluan.uit@gmail.com>
2026-08-28 15:21:11 +0700

    Disable the Chrome password breach dialog in system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ]
+  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ] do |options|
+    options.add_argument("--disable-features=PasswordLeakDetection")
+    options.add_preference("profile.password_manager_leak_detection", false)
+    options.add_preference("profile.password_manager_enabled", false)
+    options.add_preference("credentials_enable_service", false)
+  end
 
   def sign_in_as(user, password: "password123")
     visit new_user_session_path
