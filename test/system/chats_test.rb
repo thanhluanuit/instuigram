c437a89d99b19f9a4f77c265df670fe05e84c88a
thanhluanuit <thanhluan.uit@gmail.com>
2026-08-28 15:21:11 +0700

    Cover sending a message and opening a conversation in system tests

--- test/system/chats_test.rb
+++ test/system/chats_test.rb
@@ -1,27 +1,25 @@
 require "application_system_test_case"
 
 class ChatsTest < ApplicationSystemTestCase
-  test "messaging another user from their profile shows the message in the conversation" do
+  test "sending a message adds it to the conversation without a page reload" do
+    sign_in_as users(:one)
+
+    visit conversation_path(conversations(:one_and_two))
+    assert_selector ".chat-message--mine", wait: 10
+
+    fill_in "message_body", with: "Hello from the system test"
+    click_button "Send"
+
+    assert_text "Hello from the system test", wait: 10
+    assert_equal conversation_path(conversations(:one_and_two)), current_path
+  end
+
+  test "the message button on a profile opens a conversation with that user" do
     sign_in_as users(:one)
 
     visit user_path(users(:two))
     click_button "Message"
 
-    fill_in "message_body", with: "Hello from the system test"
-    click_button "Send"
-
-    assert_text "Hello from the system test"
-  end
-
-  test "opening a conversation from the inbox clears the unread badge" do
-    sign_in_as users(:two)
-
-    assert_selector "#unread-badge", text: "3"
-
-    visit conversations_path
-    find("#conversation_#{conversations(:one_and_two).id}").click
-
-    assert_current_path conversation_path(conversations(:one_and_two))
-    assert_no_selector "#unread-badge", text: "3"
+    assert_current_path conversation_path(conversations(:one_and_two)), wait: 10
   end
 end
