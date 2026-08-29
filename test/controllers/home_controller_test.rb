bf029670d25ac3f2845f09dbc4861fb72acf0082
thanhluanuit <thanhluan.uit@gmail.com>
2026-08-29 11:14:27 +0700

    Show a follow button in the post header

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -84,6 +84,24 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
     assert_select ".reaction-icon.liked", count: 1
   end
 
+  test "shows a follow button on other users' posts and none on your own" do
+    sign_in users(:one)
+
+    get root_path
+
+    assert_select "form[data-follow-user-id=?] button", users(:two).id.to_s, text: "Follow"
+    assert_select "form[data-follow-user-id=?]", users(:one).id.to_s, count: 0
+  end
+
+  test "shows the following state on posts by a user you already follow" do
+    Follows::Create.call(follower: users(:one), followed: users(:two))
+    sign_in users(:one)
+
+    get root_path
+
+    assert_select "form[data-follow-user-id=?] button", users(:two).id.to_s, text: "Following"
+  end
+
   test "shows a comment icon opening the post popup for each post" do
     sign_in users(:one)
 
