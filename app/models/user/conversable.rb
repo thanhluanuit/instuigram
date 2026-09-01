08745f3e28f93ea775b7bdb6b1de4d83929e8900
thanhluanuit <thanhluan.uit@gmail.com>
2026-09-01 16:16:49 +0700

    Add frozen_string_literal to the new User concerns

--- app/models/user/conversable.rb
+++ app/models/user/conversable.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module User::Conversable
   extend ActiveSupport::Concern
 
--- app/models/user/followable.rb
+++ app/models/user/followable.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module User::Followable
   extend ActiveSupport::Concern
 
