f9a3a128bb17cdf9f8ab5bcabb0b7ba3262b7d5b
thanhluanuit <thanhluan.uit@gmail.com>
2026-09-01 16:08:51 +0700

    Extract User::Followable and User::Conversable concerns

---
+++ app/models/user/conversable.rb
@@ -0,0 +1,21 @@
+module User::Conversable
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :conversation_participants
+    has_many :conversations, through: :conversation_participants
+    has_many :messages, dependent: :destroy
+
+    before_destroy :destroy_conversations
+  end
+
+  def unread_messages_count
+    conversation_participants.sum(:unread_count)
+  end
+
+  private
+
+  def destroy_conversations
+    Conversation.where(id: conversations.ids).destroy_all
+  end
+end
