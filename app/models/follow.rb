4bea8d49ca802e79d68f1b7045707d17d391b597
thanhluanuit <thanhluan.uit@gmail.com>
2026-09-02 10:21:35 +0700

    Drop the Check Constraints section from model annotations

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -8,10 +8,6 @@
 #  followed_id :bigint           not null
 #  follower_id :bigint           not null
 #
-# Check Constraints
-#
-#  follows_no_self_follow  (follower_id <> followed_id)
-#
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User", counter_cache: :following_count
   belongs_to :followed, class_name: "User", counter_cache: :followers_count
