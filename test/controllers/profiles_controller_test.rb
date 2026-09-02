847457f5806421b3e184a1dd9e3b66e265377b42
thanhluanuit <thanhluan.uit@gmail.com>
2026-09-02 15:12:05 +0700

    Cover the website scheme allow-list from the model through the rendered profile link

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -116,4 +116,17 @@ class ProfilesControllerTest < ActionDispatch::IntegrationTest
 
     assert_not_equal "not-an-email", users(:one).reload.email
   end
+
+  test "update rejects a website that smuggles a javascript: URI, leaving the existing link intact" do
+    sign_in users(:one)
+    users(:one).update!(website: "https://safe.example")
+
+    patch profile_path, params: { user: { website: "https://example.com\njavascript:alert(document.cookie)" } }
+
+    assert_equal "https://safe.example", users(:one).reload.website
+
+    get profile_path
+
+    assert_select "a.profile-header__website[href=?]", "https://safe.example"
+  end
 end
