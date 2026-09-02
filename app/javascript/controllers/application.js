c6b90f09c66a4a28315004e4b00f16e7cc6514b7
thanhluanuit <thanhluan.uit@gmail.com>
2026-09-02 16:24:43 +0700

    Remove comment code

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,7 +2,6 @@ import { Application } from "@hotwired/stimulus"
 
 const application = Application.start()
 
-// Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
 
