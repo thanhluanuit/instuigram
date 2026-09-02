f0a1f3fd172eb2d6a6dbfdcff82045f93a8cf7ef
thanhluanuit <thanhluan.uit@gmail.com>
2026-09-02 10:22:37 +0700

    Reorder Post's members to match the User model

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,18 +15,18 @@ class Post < ApplicationRecord
   include Imageable
   include HashTaggable
 
+  DESCRIPTION_LIMIT = 2200
+
   belongs_to :user
+  has_many :comments, dependent: :destroy
+  has_many :reactions, as: :reactable, dependent: :destroy
+
+  validates :description, length: { maximum: DESCRIPTION_LIMIT }
+
   scope :created_recently, -> { order(created_at: :desc) }
   scope :discoverable_for, ->(user) {
     where.not(user_id: user.id)
       .where.not(user_id: Follow.select(:followed_id).where(follower_id: user.id))
       .order(Arel.sql("posts.reactions_count + posts.comments_count DESC"), created_at: :desc)
   }
-
-  has_many :comments, dependent: :destroy
-  has_many :reactions, as: :reactable, dependent: :destroy
-
-  DESCRIPTION_LIMIT = 2200
-
-  validates :description, length: { maximum: DESCRIPTION_LIMIT }
 end
